c3a3f5213e636496b8e31ff5244249dcab045133
Kumonda221 <kumonda@kucro3.org>
2023-02-13 15:04:44 +0800

    Commit EventBus components

---
+++ main/eventbus/eventbus.hpp
@@ -0,0 +1,323 @@
+#pragma once
+//
+// BullsEye EventBus
+//
+//
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <type_traits>
+
+
+#define CERR_EB3001             "EB-3001: The dedicated type _TEvent is not subclass of Event<_TEvent>"
+
+
+namespace BullsEye {
+
+    // --> essentials
+
+    // Pre-declarations
+    template<class _TEvent> class Event;
+    template<class _TEvent> class EventListener;
+    template<class _TEvent> class EventBus;
+
+
+    // Event Instance Interface
+    template<class _TEvent>
+    class Event {
+        static_assert(std::is_convertible_v<_TEvent, Event<_TEvent>>, CERR_EB3001);
+
+    public:
+        static EventBus<_TEvent>&   GetEventBus() noexcept;
+
+        static void                 Register(const EventListener<_TEvent>& listener) noexcept;
+        static void                 Register(EventListener<_TEvent>&& listener) noexcept;
+
+        static int                  Unregister(const std::string& name) noexcept;
+        static bool                 UnregisterOnce(const std::string& name) noexcept;
+        static void                 UnregisterAll() noexcept;
+
+    public:
+        virtual ~Event() noexcept;
+
+        virtual const std::string&  GetName() const noexcept = 0;
+
+        void                        Fire() final;
+        void                        Fire(EventBus<_TEvent>& eventbus) final;
+    };
+
+
+    // Event Handler
+    template<class _TEvent>
+    class EventListener {
+        static_assert(std::is_convertible_v<_TEvent, Event<_TEvent>>, CERR_EB3001);
+
+    private:
+        const std::string   name;
+        const int           priority;
+
+    public:
+        EventListener(const std::string& name, int priority) noexcept;
+        virtual ~EventListener() noexcept;
+
+        const std::string&          GetName() const noexcept;
+        int                         GetPriority() const noexcept;
+
+        virtual void                OnEvent(_TEvent& event) = 0;
+    };
+
+    // Event Bus
+    template<class _TEvent>
+    class EventBus {
+        static_assert(std::is_convertible_v<_TEvent, Event<_TEvent>>, CERR_EB3001);
+
+    private:
+        std::vector<EventListener<_TEvent>>  list;
+
+    private:
+        std::vector<EventListener<_TEvent>>::const_iterator _NextPos(int priority) noexcept;
+
+    public:
+        EventBus() noexcept;
+        ~EventBus() noexcept;
+
+        void                        Register(const EventListener<_TEvent>& listener) noexcept;
+        void                        Register(EventListener<_TEvent>&& listener) noexcept;
+
+        int                         Unregister(const std::string& name) noexcept;
+        bool                        UnregisterOnce(const std::string& name) noexcept;
+        void                        UnregisterAll() noexcept;
+
+        void                        FireEvent(_TEvent& event);
+    };
+
+
+
+    // --> utilities
+
+    // Event Handler Functional Stub
+    template<class _TEvent>
+    class EventListenerFunctionalStub : public EventListener<_TEvent> {
+    private:
+        std::function<void(_TEvent&)>   func;
+
+    public:
+        EventListenerFunctionalStub(const std::string& name, int priority, std::function<void(_TEvent&)> func) noexcept;
+        virtual ~EventListenerFunctionalStub() noexcept;
+
+        virtual void                OnEvent(_TEvent& event) override;
+    };
+
+    template<class _TEvent>
+    using FunctionalEventListener = EventListenerFunctionalStub<_TEvent>;
+}
+
+
+
+// Implementation of: template<class _TEvent> class Event
+namespace BullsEye {
+
+    //
+    template<class _TEvent>
+    inline static EventBus<_TEvent>& Event<_TEvent>::GetEventBus() noexcept
+    {
+        static EventBus<_TEvent> global_eventbus;
+
+        return global_eventbus;
+    }
+
+    template<class _TEvent>
+    inline static void Event<_TEvent>::Register(const EventListener<_TEvent>& listener) noexcept
+    {
+        GetEventBus().Register(listener);
+    }
+
+    template<class _TEvent>
+    inline static void Event<_TEvent>::Register(EventListener<_TEvent>&& listener) noexcept
+    {
+        GetEventBus().Register(listener);
+    }
+
+    template<class _TEvent>
+    inline static int Event<_TEvent>::Unregister(const std::string& name) noexcept
+    {
+        return GetEventBus().Unregister(name);
+    }
+
+    template<class _TEvent>
+    inline static bool Event<_TEvent>::UnregisterOnce(const std::string& name) noexcept
+    {
+        return GetEventBus().UnregisterOnce(name);
+    }
+
+    template<class _TEvent>
+    inline static void Event<_TEvent>::UnregisterAll() noexcept
+    {
+        GetEventBus().UnregisterAll();
+    }
+
+    //
+    template<class _TEvent>
+    Event<_TEvent>::~Event() noexcept
+    { }
+
+    template<class _TEvent>
+    inline void Event<_TEvent>::Fire()
+    {
+        GetEventBus().FireEvent(*this);
+    }
+
+    template<class _TEvent>
+    inline void Event<_TEvent>::Fire(EventBus<_TEvent>& eventbus)
+    {
+        eventbus.FireEvent(*this);
+    }
+}
+
+
+// Implementation of: template<class _TEvent> class EventListener
+namespace BullsEye {
+    //
+    // const std::string   name;
+    // const int           priority;
+    //
+
+    template<class _TEvent>
+    EventListener<_TEvent>::EventListener(const std::string& name, int priority) noexcept
+        : name      (name)
+        , priority  (priority)
+    { }
+
+    template<class _TEvent>
+    EventListener<_TEvent>::~EventListener() noexcept
+    { }
+
+    template<class _TEvent>
+    inline const std::string& EventListener<_TEvent>::GetName() const noexcept
+    {
+        return name;
+    }
+
+    template<class _TEvent>
+    inline int EventListener<_TEvent>::GetPriority() const noexcept
+    {
+        return priority;
+    }
+}
+
+
+// Implementation of: template<class _TEvent> class EventBus
+namespace BullsEye {
+    //
+    // std::vector<EventListener<_TEvent>>  list;
+    //
+
+    template<class _TEvent>
+    std::vector<EventListener<_TEvent>>::const_iterator EventBus<_TEvent>::_NextPos(int priority) noexcept
+    {
+        auto iter = list.begin();
+        for (; iter != list.end(); iter++)
+            if (iter->GetPriority() > listener.GetPriority())
+                break;
+
+        return iter;
+    }
+
+    template<class _TEvent>
+    EventBus<_TEvent>::EventBus() noexcept
+        : list  ()
+    { }
+
+    template<class _TEvent>
+    EventBus<_TEvent>::~EventBus() noexcept
+    { }
+    
+    template<class _TEvent>
+    inline void EventBus<_TEvent>::Register(const EventListener<_TEvent>& listener) noexcept
+    {
+        list.insert(_NextPos(listener.GetPriority()), listener);
+    }
+
+    template<class _TEvent>
+    inline void EventBus<_TEvent>::Register(EventListener<_TEvent>&& listener) noexcept
+    {
+        list.insert(_NextPos(listener.GetPriority()), listener);
+    }
+
+    template<class _TEvent>
+    int EventBus<_TEvent>::Unregister(const std::string& name) noexcept
+    {
+        auto epos = std::remove_if(list.begin(), list.end(), 
+            [name](const EventListener<_TEvent>& obj) -> bool {
+                return obj.GetName() == name;
+            });
+
+        int count = std::distance(epos, list.end());
+
+        list.erase(epos, list.end());
+
+        return count;
+    }
+
+    template<class _TEvent>
+    bool EventBus<_TEvent>::UnregisterOnce(const std::string& name) noexcept
+    {
+        bool found;
+
+        auto epos = list.begin();
+        for (; epos != list.end(); epos++)
+            if (epos->GetName() == name)
+            {
+                found = true;
+                break;
+            }
+        
+        if (found)
+            list.erase(epos);
+
+        return found;
+    }
+
+    template<class _TEvent>
+    inline void EventBus<_TEvent>::UnregisterAll() noexcept
+    {
+        list.clear();
+        list.shrink_to_fit();
+    }
+    
+    template<class _TEvent>
+    void EventBus<_TEvent>::FireEvent(_TEvent& event)
+    {
+        for (EventListener<_TEvent> listener : list)
+            listener.OnEvent(event);
+    }
+}
+
+
+// Implementation of: template<class _TEvent> class EventListenerFunctionalStub
+namespace BullsEye {
+    //
+    // std::function<void(_TEvent&)>   func;
+    //
+
+    template<class _TEvent>
+    EventListenerFunctionalStub<_TEvent>::EventListenerFunctionalStub(
+            const std::string&              name, 
+            int                             priority, 
+            std::function<void(_TEvent&)>   func) noexcept
+        : EventListener(name, priority)
+        , func  (func)
+    { }
+
+    template<class _TEvent>
+    EventListenerFunctionalStub<_TEvent>::~EventListenerFunctionalStub() noexcept
+    { }
+
+    template<class _TEvent>
+    void EventListenerFunctionalStub<_TEvent>::OnEvent(_TEvent& event)
+    {
+        return func(event);
+    }
+}
