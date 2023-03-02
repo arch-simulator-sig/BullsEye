5da6835d2995c99d517ffa76bd955b0a7a03568c
Kumonda221 <kumonda@kucro3.org>
2023-03-02 18:01:20 +0800

    Commit MIPS32 Tracer components

---
+++ main/nscscc_single/isa/base/mips32trace.hpp
@@ -0,0 +1,183 @@
+#pragma once
+//
+// MIPS32 (NSCSCC) ISA Emulator (Jasse)
+//
+// Trace Subsystem Infrastructures
+//
+
+#include <memory>
+
+#include "mips32def.hpp"
+#include "mips32decode.hpp"
+
+#include "common/objpool.hpp"
+
+
+namespace Jasse {
+
+    // MIPS32 Trace Entity
+    class MIPS32TraceEntity {
+    public:
+        using Pool      = BullsEye::ObjectPool<MIPS32TraceEntity>;
+        using Reference = BullsEye::ObjectPool<MIPS32TraceEntity>::Reference;
+
+    private:
+        MIPS32Instruction   insn;
+        pc_t                pc;
+
+        arch32_t            value;
+
+        Reference           op0;
+        Reference           op1;
+
+    public:
+        MIPS32TraceEntity(pc_t pc, const MIPS32Instruction& insn, arch32_t value) noexcept;
+        MIPS32TraceEntity(pc_t pc, const MIPS32Instruction& insn, arch32_t value, const Reference& op0) noexcept;
+        MIPS32TraceEntity(pc_t pc, const MIPS32Instruction& insn, arch32_t value, const Reference& op0, const Reference& op1) noexcept;
+
+        void                        SetInstruction(const MIPS32Instruction& insn) noexcept;
+        void                        SetPC(pc_t pc) noexcept;
+        void                        SetValue(arch32_t value) noexcept;
+
+        void                        SetFirstOperand(const Reference& trace) noexcept;
+        void                        SetSecondOperand(const Reference& trace) noexcept;
+
+        MIPS32Instruction&          GetInstruction() noexcept;
+        const MIPS32Instruction&    GetInstruction() const noexcept;
+        pc_t                        GetPC() const noexcept;
+        arch32_t                    GetValue() const noexcept;
+
+        Reference&                  GetFirstOperand() noexcept;
+        const Reference&            GetFirstOperand() const noexcept;
+        Reference&                  GetSecondOperand() noexcept;
+        const Reference&            GetSecondOperand() const noexcept;
+    };
+
+
+    // MIPS32 GPR Tracer
+    class MIPS32GPRTracer {
+    private:
+        static constexpr int    SIZE    = MIPS32_ARCH_REG_COUNT;
+    };
+    
+    // MIPS32 Memory Tracer
+    class MIPS32MemoryTracer {
+
+    };
+}
+
+
+// Implementation of: class MIPS32TraceEntity
+namespace Jasse {
+    //
+    // MIPS32Instruction   insn;
+    // pc_t                pc;
+    //
+    // arch32_t            value;
+    //
+    // Reference           op0;
+    // Reference           op1;
+    //
+
+    MIPS32TraceEntity::MIPS32TraceEntity(
+            pc_t                        pc, 
+            const MIPS32Instruction&    insn, 
+            arch32_t                    value) noexcept
+        : pc    (pc)
+        , insn  (insn)
+        , value (value)
+        , op0   ()
+        , op1   ()
+    { }
+
+    MIPS32TraceEntity::MIPS32TraceEntity(
+            pc_t                        pc,
+            const MIPS32Instruction&    insn,
+            arch32_t                    value,
+            const Reference&            op0) noexcept
+        : pc    (pc)
+        , insn  (insn)
+        , value (value)
+        , op0   (op0)
+        , op1   ()
+    { }
+
+    MIPS32TraceEntity::MIPS32TraceEntity(
+            pc_t pc,
+            const MIPS32Instruction&    insn,
+            arch32_t                    value,
+            const Reference&            op0,
+            const Reference&            op1) noexcept
+        : pc    (pc)
+        , insn  (insn)
+        , value (value)
+        , op0   (op0)
+        , op1   (op1)
+    { }
+
+    inline void MIPS32TraceEntity::SetInstruction(const MIPS32Instruction& insn) noexcept
+    {
+        this->insn = insn;
+    }
+
+    inline void MIPS32TraceEntity::SetPC(pc_t pc) noexcept
+    {
+        this->pc = pc;
+    }
+
+    inline void MIPS32TraceEntity::SetValue(arch32_t value) noexcept
+    {
+        this->value = value;
+    }
+
+    inline void MIPS32TraceEntity::SetFirstOperand(const Reference& op0) noexcept
+    {
+        this->op0 = op0;
+    }
+
+    inline void MIPS32TraceEntity::SetSecondOperand(const Reference& op1) noexcept
+    {
+        this->op1 = op1;
+    }
+
+    inline MIPS32Instruction& MIPS32TraceEntity::GetInstruction() noexcept
+    {
+        return insn;
+    }
+
+    inline const MIPS32Instruction& MIPS32TraceEntity::GetInstruction() const noexcept
+    {
+        return insn;
+    }
+
+    inline pc_t MIPS32TraceEntity::GetPC() const noexcept
+    {
+        return pc;
+    }
+
+    inline arch32_t MIPS32TraceEntity::GetValue() const noexcept
+    {
+        return value;
+    }
+
+    inline MIPS32TraceEntity::Reference& MIPS32TraceEntity::GetFirstOperand() noexcept
+    {
+        return op0;
+    }
+
+    inline const MIPS32TraceEntity::Reference& MIPS32TraceEntity::GetFirstOperand() const noexcept
+    {
+        return op0;
+    }
+
+    inline MIPS32TraceEntity::Reference& MIPS32TraceEntity::GetSecondOperand() noexcept
+    {
+        return op1;
+    }
+
+    inline const MIPS32TraceEntity::Reference& MIPS32TraceEntity::GetSecondOperand() const noexcept
+    {
+        return op1;
+    }
+}
+
