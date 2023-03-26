674a3b141f7ad0f8000a8ebc5fd86e343239aae6
Kumonda221 <kumonda@kucro3.org>
2023-03-26 14:14:14 +0800

    Embedded MIPS32-NSCSCC ISA instruction execution event procedure into executors

--- main/nscscc_single/isa/mips32_nscscc/mips32_nscscc_impl_execute.hpp
+++ main/nscscc_single/isa/mips32_nscscc/mips32_nscscc_impl_execute.hpp
@@ -6,6 +6,7 @@
 //
 
 #include "mips32_nscscc_def_execute.hpp"
+#include "mips32_nscscc_def_event.hpp"
 
 
 #define RS                          inst.Arch().GPRs()[insn.GetRS()]
@@ -56,6 +57,17 @@
 }
 
 
+#define event_wrapped(name, expr) \
+    { \
+        MIPS32Instruction __insn = insn; \
+        if (MIPS32PreExecutionEvents::name(inst, PC, __insn).Fire().IsCancelled()) \
+            return { EXEC_EMULATION_CANCELLED, ECANCELED }; \
+        auto __expr = [&](MIPS32Instruction& insn) -> MIPS32ExecOutcome { expr }; \
+        MIPS32ExecOutcome outcome = __expr(__insn); \
+        return (MIPS32PostExecutionEvents::name(inst, PC, __insn, outcome).Fire().GetOutcome()); \
+    } \
+
+
 #define implexec(name, expr) \
     MIPS32ExecOutcome MIPS32_EXECUTOR(name)(MIPS32_EXECUTOR_PARAMS) expr
 
@@ -64,159 +76,159 @@ namespace Jasse {
 
     // ADD rd, rs, rt
     implexec(ADD, 
-        noexcept sequential(RD = RS + RT));
+        noexcept event_wrapped(ADD, sequential(RD = RS + RT)));
 
     // ADDI rt, rs, imm
     implexec(ADDI,
-        noexcept sequential(RT = RS + IMM16_SEXT));
+        noexcept event_wrapped(ADDI, sequential(RT = RS + IMM16_SEXT)));
 
     // ADDU rd, rs, rt
     implexec(ADDU,
-        noexcept sequential(RD = RS + RT));
+        noexcept event_wrapped(ADDU, sequential(RD = RS + RT)));
 
     // ADDIU rt, rs, imm
     implexec(ADDIU,
-        noexcept sequential(RT = RS + IMM16_SEXT));
+        noexcept event_wrapped(ADDIU, sequential(RT = RS + IMM16_SEXT)));
 
     // SUB rd, rs, rt
     implexec(SUB, 
-        noexcept sequential(RD = RS - RT));
+        noexcept event_wrapped(SUB, sequential(RD = RS - RT)));
 
     // SUBU rd, rs, rt
     implexec(SUBU,
-        noexcept sequential(RD = RS - RT));
+        noexcept event_wrapped(SUBU, sequential(RD = RS - RT)));
 
     // SLT rd, rs, rt
     implexec(SLT,
-        noexcept sequential(RD = (signed) RS < (signed) RT));
+        noexcept event_wrapped(SLT, sequential(RD = (signed) RS < (signed) RT)));
 
     // SLTI rt, rs, imm
     implexec(SLTI,
-        noexcept sequential(RT = (signed) RS < (signed) IMM16_SEXT));
+        noexcept event_wrapped(SLTI, sequential(RT = (signed) RS < (signed) IMM16_SEXT)));
 
     // SLTU rd, rs, rt
     implexec(SLTU,
-        noexcept sequential(RD = RS < RT));
+        noexcept event_wrapped(SLTU, sequential(RD = RS < RT)));
 
     // SLTIU rt, rs, imm
     implexec(SLTIU,
-        noexcept sequential(RD = RS < IMM16_ZEXT));
+        noexcept event_wrapped(SLTIU, sequential(RD = RS < IMM16_ZEXT)));
 
     // MUL rd, rs, rt
     implexec(MUL,
-        noexcept sequential(RD = RS * RT));
+        noexcept event_wrapped(MUL, sequential(RD = RS * RT)));
 
 
     // AND rd, rs, rt
     implexec(AND,
-        noexcept sequential(RD = RS & RT));
+        noexcept event_wrapped(AND, sequential(RD = RS & RT)));
 
     // ANDI rt, rs, imm
     implexec(ANDI,
-        noexcept sequential(RT = RS & IMM16_ZEXT));
+        noexcept event_wrapped(ANDI, sequential(RT = RS & IMM16_ZEXT)));
 
     // LUI rt, imm
     implexec(LUI,
-        noexcept sequential(RT = IMM16_ZEXT << 16));
+        noexcept event_wrapped(LUI, sequential(RT = IMM16_ZEXT << 16)));
 
     // OR rd, rs, rt
     implexec(OR,
-        noexcept sequential(RD = RS | RT));
+        noexcept event_wrapped(OR, sequential(RD = RS | RT)));
 
     // ORI rt, rs, imm
     implexec(ORI,
-        noexcept sequential(RT = RS | IMM16_ZEXT));
+        noexcept event_wrapped(ORI, sequential(RT = RS | IMM16_ZEXT)));
 
     // XOR rd, rs, rt
     implexec(XOR,
-        noexcept sequential(RD = RS ^ RT));
+        noexcept event_wrapped(XOR, sequential(RD = RS ^ RT)));
 
     // XORI rt, rs, imm
     implexec(XORI,
-        noexcept sequential(RT = RS ^ IMM16_ZEXT));
+        noexcept event_wrapped(XORI, sequential(RT = RS ^ IMM16_ZEXT)));
 
 
     // SLL rd, rt, sa
     implexec(SLL,
-        noexcept sequential(RD = RT << SA));
+        noexcept event_wrapped(SLL, sequential(RD = RT << SA)));
 
     // SLLV rd, rt, rs
     implexec(SLLV,
-        noexcept sequential(RD = RT << RS));
+        noexcept event_wrapped(SLLV, sequential(RD = RT << RS)));
 
     // SRA rd, rt, sa
     implexec(SRA,
-        noexcept sequential(RD = (signed) RT >> SA));
+        noexcept event_wrapped(SRA, sequential(RD = (signed) RT >> SA)));
     
     // SRAV rd, rt, rs
     implexec(SRAV,
-        noexcept sequential(RD = (signed) RT >> RS));
+        noexcept event_wrapped(SRAV, sequential(RD = (signed) RT >> RS)));
 
     // SRL rd, rt, sa
     implexec(SRL,
-        noexcept sequential(RD = RT >> SA));
+        noexcept event_wrapped(SRL, sequential(RD = RT >> SA)));
 
     // SRLV rd, rt, rs
     implexec(SRLV,
-        noexcept sequential(RD = RT >> RS));
+        noexcept event_wrapped(SRLV, sequential(RD = RT >> RS)));
 
 
     // BEQ rs, rt, offset
     implexec(BEQ,
-        noexcept branch_if(RS == RT));
+        noexcept event_wrapped(BEQ, branch_if(RS == RT)));
 
     // BNE rs, rt, offset
     implexec(BNE,
-        noexcept branch_if(RS != RT));
+        noexcept event_wrapped(BNE, branch_if(RS != RT)));
 
     // BGEZ rs, offset
     implexec(BGEZ,
-        noexcept branch_if(RS >= 0));
+        noexcept event_wrapped(BGEZ, branch_if(RS >= 0)));
 
     // BGTZ rs, offset
     implexec(BGTZ,
-        noexcept branch_if(RS > 0));
+        noexcept event_wrapped(BGTZ, branch_if(RS > 0)));
 
     // BLEZ rs, offset
     implexec(BLEZ,
-        noexcept branch_if(RS <= 0));
+        noexcept event_wrapped(BLEZ, branch_if(RS <= 0)));
 
     // BLTZ rs, offset
     implexec(BLTZ,
-        noexcept branch_if(RS < 0));
+        noexcept event_wrapped(BLTZ, branch_if(RS < 0)));
     
     // J target
     implexec(J,
-        noexcept jump((IMM26_ZEXT << 2) | (PC & 0xF0000000)));
+        noexcept event_wrapped(J, jump((IMM26_ZEXT << 2) | (PC & 0xF0000000))));
 
     // JAL target
     implexec(JAL,
-        noexcept jumplink((IMM26_ZEXT << 2) | (PC & 0xF0000000), R31));
+        noexcept event_wrapped(JAL, jumplink((IMM26_ZEXT << 2) | (PC & 0xF0000000), R31)));
 
     // JR rs
     implexec(JR,
-        noexcept jump(RS));
+        noexcept event_wrapped(JR, jump(RS)));
 
     // JALR rd, rs
     implexec(JALR,
-        noexcept jumplink(RS, RD));
+        noexcept event_wrapped(JALR, jumplink(RS, RD)));
 
 
     // LB rt, offset(base)
     implexec(LB,
-        noexcept load(RT, BYTE));
+        noexcept event_wrapped(LB, load(RT, BYTE)));
 
     // LW rt, offset(base)
     implexec(LW,
-        noexcept load(RT, WORD));
+        noexcept event_wrapped(LW, load(RT, WORD)));
 
     // SB rt, offset(base)
     implexec(SB,
-        noexcept store(RT, BYTE));
+        noexcept event_wrapped(SB, store(RT, BYTE)));
 
     // SW rt, offset(base)
     implexec(SW,
-        noexcept store(RT, WORD));
+        noexcept event_wrapped(SW, store(RT, WORD)));
 }
 
 
