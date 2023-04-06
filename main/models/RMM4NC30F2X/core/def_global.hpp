0f762baebef954597a83654685592aa357a7f6ee
Kumonda221 <kumonda@kucro3.org>
2023-04-06 21:00:47 +0800

    Commit core model global definitions (Fetch stage)

---
+++ main/models/RMM4NC30F2X/core/def_global.hpp
@@ -0,0 +1,12 @@
+#pragma once
+//
+// Core global definitions
+//
+//
+
+
+#define FETCH_PREDECODED_NORMAL           0b0000
+
+#define FETCH_PREDECODED_BRANCH           0b0100
+#define FETCH_PREDECODED_JUMP             0b0010
+#define FETCH_PREDECODED_JUMP_LINK        0b0011
