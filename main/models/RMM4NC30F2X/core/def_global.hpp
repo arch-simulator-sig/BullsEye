0355695f4ce9abfc23e74a59fbb7b827a8618a76
Kumonda221 <kumonda@kucro3.org>
2023-04-28 20:48:18 +0800

    Commit core model global definitions

--- main/models/RMM4NC30F2X/core/def_global.hpp
+++ main/models/RMM4NC30F2X/core/def_global.hpp
@@ -4,9 +4,49 @@
 //
 //
 
+#include <cstdint>
+
+#include "common/nonstdint.hpp"
+
+#include "models/util/mips32def.hpp"
+#include "models/util/mips32helper.hpp"
+
 
 #define FETCH_PREDECODED_NORMAL           0b0000
 
 #define FETCH_PREDECODED_BRANCH           0b0100
 #define FETCH_PREDECODED_JUMP             0b0010
 #define FETCH_PREDECODED_JUMP_LINK        0b0011
+
+
+// Type definitions
+namespace BullsEye::Gemini30F2::Global {
+
+
+
+
+    using VirtualAddress        = uint32_t;
+
+    using PhysicalAddress       = uint32_t;
+
+
+    using FID                   = uint8_t;
+
+    using RawInstruction        = uint32_t;
+
+    struct _PredecodedInstruction {
+        RawInstruction  insn;
+        uint4_t         tag;
+    };
+
+    using PredecodedInstruction = _PredecodedInstruction;
+
+    using PC                    = pc_t;
+}
+
+
+// Value configurations
+namespace BullsEye::Gemini30F2::Global {
+
+    static constexpr PC     START_UP_PC     = 0x80000000;
+}
