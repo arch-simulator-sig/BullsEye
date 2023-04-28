b7060cf393eb1c7d0a10b81acf47d6f6b03dfc69
Kumonda221 <kumonda@kucro3.org>
2023-04-28 20:49:59 +0800

    Commit core model global modules (Virtual&Physical Address Convertion)

---
+++ main/models/RMM4NC30F2X/core/stage_global.hpp
@@ -0,0 +1,43 @@
+#pragma once
+//
+// Stage Global models for RMM4NC30F2X (Gemini 30F2)
+//
+//
+//
+
+#include <cstdint>
+
+#include "common/nonstdint.hpp"
+
+
+#include "def_global.hpp"
+
+
+namespace BullsEye::Gemini30F2::Global {
+
+    class VPAddressConvertion {
+    public:
+        struct PhysicalAddressBundle {
+            Global::PhysicalAddress paddr;
+            bool                    kseg1;
+        };
+
+    public:
+        PhysicalAddressBundle   Comb(Global::VirtualAddress vaddr) const noexcept;
+    };
+}
+
+
+// Implementation of: class VPAddressConvertion
+namespace BullsEye::Gemini30F2::Global {
+
+    inline VPAddressConvertion::PhysicalAddressBundle VPAddressConvertion::Comb(Global::VirtualAddress vaddr) const noexcept
+    {
+        PhysicalAddressBundle result;
+
+        result.paddr = vaddr & 0x1FFFFFFF;
+        result.kseg1 = ((vaddr & 0xE0000000) == 0xA0000000) || ((vaddr & 0xE0000000) == 0xB0000000);
+
+        return result;
+    }
+}
