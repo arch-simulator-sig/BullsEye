77576afeb0846bab9ac1449ba602a3d50c509ddd
Kumonda221 <kumonda@kucro3.org>
2023-10-02 00:39:25 +0800

    Commit BullsEye common utilities

---
+++ main/common/util.hpp
@@ -0,0 +1,30 @@
+#pragma once
+//
+// BullsEye Utility
+//
+//
+
+
+#include <string>
+#include <algorithm>
+
+
+namespace BullsEye {
+
+    inline char* NewStringAndCopy(const char* src, size_t length) noexcept
+    {
+        char* newString = new char[length + 1];
+        std::memcpy(newString, src, sizeof(char) * (length + 1));
+        return newString;
+    }
+
+    inline char* NewStringAndCopy(const char* src) noexcept
+    {
+        return NewStringAndCopy(src, std::strlen(src));
+    }
+
+    inline char* NewStringAndCopy(std::string src) noexcept
+    {
+        return NewStringAndCopy(src.c_str(), src.length());
+    }
+}
