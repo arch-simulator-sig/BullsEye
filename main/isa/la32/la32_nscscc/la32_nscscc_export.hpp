90465b70c32dfbb4a4c72157c3229c56cda26167
Kumonda221 <kumonda@kucro3.org>
2023-11-10 00:52:42 +0800

    Include file IWYU pragma

--- main/isa/la32/la32_nscscc/la32_nscscc_export.hpp
+++ main/isa/la32/la32_nscscc/la32_nscscc_export.hpp
@@ -9,11 +9,11 @@
 #define __HEADER_JASSE_LA32__EXT_NSCSCC_EXPORT
 
 
-#include "la32_nscscc_def_encode.hpp"
+#include "la32_nscscc_def_encode.hpp"       // IWYU pragma: export
 
-#include "la32_nscscc_def_decode.hpp"
-#include "la32_nscscc_def_execute.hpp"
-#include "la32_nscscc_def_event.hpp"
+#include "la32_nscscc_def_decode.hpp"       // IWYU pragma: export
+#include "la32_nscscc_def_execute.hpp"      // IWYU pragma: export
+#include "la32_nscscc_def_event.hpp"        // IWYU pragma: export
 
 
 #endif
